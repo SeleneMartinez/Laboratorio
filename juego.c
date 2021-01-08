01bb6beaa25510b887c6fff7f70bb84f72b36087
Selene Martinez <semartinez@fi.uba.ar>
2021-01-08 01:37:10 -0300

    updates

--- juego.c
+++ juego.c
@@ -1,9 +1,22 @@
 #include "utiles.h"
+#include "detector_personajes.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
+void inicializar_personaje(personaje_t* personaje);
+
+void inicializar_personaje(personaje_t* personaje){
+    detectar_personaje(personaje->tipo);
+    pers
+}
+
 int main(){
+
+    juego_t juego;
+    personaje_t personaje;
+
+    
 
 
     return 0;
