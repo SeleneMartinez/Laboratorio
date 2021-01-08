ace2a6e1e0cf0313e10dc88acf6ce5adae776341
Selene Martinez <semartinez@fi.uba.ar>
2021-01-08 01:39:50 -0300

    updates

---
+++ escape_laboratorio.c
@@ -0,0 +1,8 @@
+#include "escape_laboratorio.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void inicializar_nivel(nivel_t* nivel, int numero_nivel, int cantidad_baldosas_pinches, int cantidad_guardia, bool hay_bomba){
+    
+}
